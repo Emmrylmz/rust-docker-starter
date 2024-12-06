73311586ed4a4957dcbfbc6b82fae316f7e4bc7a
Emmrylmz <88165258+Emmrylmz@users.noreply.github.com>
2024-12-06 14:12:44 +0300

    initial

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-fn return_reference(some_string: &String) -> &String {
-    some_string
-} // some_string goes out of scope, but reference_to_s still points to the memory location
-
 fn main() {
-    let s = String::from("Hello");
-    let reference_to_s = return_reference(&s);
-    println!("{}", reference_to_s);
-    println!("HEY {}", reference_to_s)
+    println!("Hello World!")
 }
