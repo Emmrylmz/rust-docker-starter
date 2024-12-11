351f057295a8071c997fa01171039c86e2f351b4
Emmrylmz <88165258+Emmrylmz@users.noreply.github.com>
2024-12-11 13:27:26 +0300

    task1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+fn modify_string(s1: &String, s2: &String) -> String {
+    s1.to_owned() + s2
+}
+
 fn main() {
-    println!("Hello World!")
+    let s1 = String::from("asd");
+    let s2 = String::from("ads");
+    println!("{}", modify_string(&s1, &s2));
 }
