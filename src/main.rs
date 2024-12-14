b886633ee36c54df51bae8dd1ef36323c47c099f
Emmrylmz <88165258+Emmrylmz@users.noreply.github.com>
2024-12-14 23:21:29 +0300

    Task Bank Account

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,61 @@
-use std::io;
-
-enum Operation {
-    Add { x: f64, y: f64 },
-    Subtract { x: f64, y: f64 },
-    Multiply { x: f64, y: f64 },
-    Divide { x: f64, y: f64 },
+trait Account {
+    fn deposit(&mut self, amount: u32);
+    fn withdraw(&mut self, amount: u32);
+    fn balance(&self) -> i32;
 }
 
-fn calculate(operation: Operation) -> f64 {
-    match operation {
-        Operation::Add { x, y } => x + y,
-        Operation::Subtract { x, y } => x - y,
-        Operation::Multiply { x, y } => x * y,
-        Operation::Divide { x, y } => {
-            if y != 0.0 {
-                x / y
-            } else {
-                panic!("Division by zero is not allowed.");
-            }
+struct BankAccount {
+    account_number: i32,
+    holder_name: String,
+    balance: i32,
+}
+
+impl Account for BankAccount {
+    fn balance(&self) -> i32 {
+        self.balance
+    }
+    fn deposit(&mut self, amount: u32) {
+        self.balance += amount as i32;
+        println!("Deposited {}. New balance: {}", amount, self.balance);
+    }
+    fn withdraw(&mut self, amount: u32) {
+        if self.balance < amount as i32 {
+            println!("Insufficient funds!")
+        } else {
+            self.balance -= amount as i32;
+            println!("Withdrawn {}. New balance: {}", amount, self.balance);
         }
     }
 }
 
 fn main() {
-    let mut input = String::new();
-
-    println!("Enter the first number:");
-    io::stdin().read_line(&mut input).unwrap();
-    let first_number: f64 = input.trim().parse().expect("Please enter a valid number.");
-    input.clear();
-
-    println!("Enter the operation (+, -, *, /):");
-    io::stdin().read_line(&mut input).unwrap();
-    let operation = input.trim().to_string();
-    input.clear();
-
-    println!("Enter the second number:");
-    io::stdin().read_line(&mut input).unwrap();
-    let second_number: f64 = input.trim().parse().expect("Please enter a valid number.");
-    input.clear();
-
-    let op = match operation.as_str() {
-        "+" => Operation::Add {
-            x: first_number,
-            y: second_number,
-        },
-        "-" => Operation::Subtract {
-            x: first_number,
-            y: second_number,
-        },
-        "*" => Operation::Multiply {
-            x: first_number,
-            y: second_number,
-        },
-        "/" => Operation::Divide {
-            x: first_number,
-            y: second_number,
-        },
-        _ => {
-            println!("Invalid operation!");
-            return;
-        }
+    let mut account1 = BankAccount {
+        account_number: 101,
+        holder_name: String::from("Emir"),
+        balance: 500,
     };
 
-    let result = calculate(op);
+    let mut account2 = BankAccount {
+        account_number: 102,
+        holder_name: String::from("Not Emir"),
+        balance: 1000,
+    };
 
-    println!("The result is: {}", result);
+    account1.deposit(300);
+
+    account2.withdraw(400);
+
+    println!(
+        "Account {} ({}): Balance is {}",
+        account1.account_number,
+        account1.holder_name,
+        account1.balance()
+    );
+
+    println!(
+        "Account {} ({}): Balance is {}",
+        account2.account_number,
+        account2.holder_name,
+        account2.balance()
+    );
 }
