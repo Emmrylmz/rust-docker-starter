d7525c399ac74ab13d4af77309a7503fa652fb77
Emmrylmz <88165258+Emmrylmz@users.noreply.github.com>
2024-12-16 03:09:50 +0300

    task 4

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,32 @@
-trait Account {
-    fn deposit(&mut self, amount: u32);
-    fn withdraw(&mut self, amount: u32);
-    fn balance(&self) -> i32;
+use std::vec;
+
+use sha2::digest::generic_array::iter;
+
+struct FilterCondition {
+    filter: Vec<i32>,
 }
 
-struct BankAccount {
-    account_number: i32,
-    holder_name: String,
-    balance: i32,
+trait FilterTrait {
+    fn is_match(&self, val: i32) -> bool;
 }
 
-impl Account for BankAccount {
-    fn balance(&self) -> i32 {
-        self.balance
+impl FilterTrait for FilterCondition {
+    fn is_match(&self, val: i32) -> bool {
+        val > 5
     }
-    fn deposit(&mut self, amount: u32) {
-        self.balance += amount as i32;
-        println!("Deposited {}. New balance: {}", amount, self.balance);
-    }
-    fn withdraw(&mut self, amount: u32) {
-        if self.balance < amount as i32 {
-            println!("Insufficient funds!")
-        } else {
-            self.balance -= amount as i32;
-            println!("Withdrawn {}. New balance: {}", amount, self.balance);
-        }
-    }
+}
+
+fn custom_filter(collection: &Vec<i32>, obj: FilterCondition) -> Vec<i32> {
+    collection
+        .iter()
+        .filter(|&&x| obj.is_match(x))
+        .cloned()
+        .collect()
 }
 
 fn main() {
-    let mut account1 = BankAccount {
-        account_number: 101,
-        holder_name: String::from("Emir"),
-        balance: 500,
-    };
-
-    let mut account2 = BankAccount {
-        account_number: 102,
-        holder_name: String::from("Not Emir"),
-        balance: 1000,
-    };
-
-    account1.deposit(300);
-
-    account2.withdraw(400);
-
-    println!(
-        "Account {} ({}): Balance is {}",
-        account1.account_number,
-        account1.holder_name,
-        account1.balance()
-    );
-
-    println!(
-        "Account {} ({}): Balance is {}",
-        account2.account_number,
-        account2.holder_name,
-        account2.balance()
-    );
+    let vector = vec![1, 2, 3, 4, 5, 6, 7, 8];
+    let condition = FilterCondition { filter: vector.clone() };
+    let new = custom_filter(&vector, condition);
+    println!("result : {:?}", new);
 }
